e241ff619fb1ae35692f94362371e70ae0cc3615
Hemanthyt <hemanth25052004@gmail.com>
2024-12-26 13:18:01 +0530

    Created a route backend of get,post,delete,put method functionality for todo

---
+++ app/api/route.js
@@ -0,0 +1,35 @@
+import { connectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/model/TodoModel";
+import { NextResponse } from "next/server";
+
+const LoadDB = async () => {
+  await connectDB();
+};
+LoadDB();
+
+export async function GET(request) {
+  const todos = await TodoModel.find({});
+  return NextResponse.json({ todos: todos });
+}
+export async function POST(request) {
+  const { title, description } = await request.json();
+  await TodoModel.create({
+    title,
+    description,
+  });
+  return NextResponse.json({ message: "Todo Created" });
+}
+
+export async function DELETE(request) {
+  const mongoId = await request.nextUrl.searchParams.get("mongoId");
+  await TodoModel.findByIdAndDelete(mongoId);
+  return NextResponse.json({ message: "Todo Deleted" });
+}
+
+export async function PUT(request) {
+  const mongoId = await request.nextUrl.searchParams.get("mongoId");
+  await TodoModel.findByIdAndUpdate(mongoId, {
+    $set: { isCompleted: true },
+  });
+  return NextResponse.json({ message: "Todo Updated" });
+}
